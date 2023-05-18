6efec8e0c51370649908942be50fe7de1f5e9ca3
tgalt <travis@galtmt.com>
2023-05-17 21:59:02 -0600

    add start work script

---
+++ terminal/Start.py
@@ -0,0 +1,29 @@
+import webbrowser
+
+# Define the websites to open in Firefox
+firefox_websites = [
+    "https://mail.google.com/",
+    "https://calendar.google.com/",
+    "https://ticktick.com/webapp/#q/all/tasks",
+    "https://login.salesforce.com/",
+]
+
+# Define the websites to open in Brave
+brave_websites = [
+    "https://chat.openai.com/",
+    "https://chat.openai.com/",
+    "https://github.com/",
+]
+
+# Define the paths to the browsers
+# The paths provided below are common paths, but they might be different on your system
+firefox_path = "/usr/bin/firefox"
+brave_path = "/usr/bin/brave-browser"
+
+# Open each website in Firefox
+for website in firefox_websites:
+    webbrowser.get(firefox_path).open_new_tab(website)
+
+# Open each website in Brave
+for website in brave_websites:
+    webbrowser.get(brave_path).open_new_tab(website)
